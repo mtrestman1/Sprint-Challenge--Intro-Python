76dcd31793995c40a683b6db4718593575f6145f
Max Trestman <mtrestman1@gmail.com>
2019-05-31 10:57:44 -0400

    oop1 completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -19,4 +19,24 @@
 # Put a comment noting which class is the base class
 
 
+class Vehicle:    #base class
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
 
